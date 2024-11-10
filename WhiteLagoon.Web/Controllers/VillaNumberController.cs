3021756ecb8054739a6f531807ebfe31f225386a
nguyentuananh921 <nguyentuananh921@gmail.com>
2024-11-10 16:07:01 +0700

    Section 6 - Update Post in Action

--- WhiteLagoon.Web/Controllers/VillaNumberController.cs
+++ WhiteLagoon.Web/Controllers/VillaNumberController.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using WhiteLagoon.Domain.Entities;
 using WhiteLagoon.Infrastructure.Data;
+using WhiteLagoon.Web.ViewModels;
 
 namespace WhiteLagoon.Web.Controllers
 {
@@ -19,28 +20,46 @@ namespace WhiteLagoon.Web.Controllers
 
         public IActionResult Index()
         {
-            var villarsNumber=_db.VillaNumbers.ToList();
-                
+            //var villarsNumber=_db.VillaNumbers.ToList();
+
+            //Loading Navigation Properties using Include.
+            var villarsNumber = _db.VillaNumbers.Include(u=>u.Villa).ToList(); 
+
             return View(villarsNumber);
         }
         #region Create
         
         public IActionResult Create()
         {
-            IEnumerable<SelectListItem> list = _db.Villas.ToList().Select(u => new SelectListItem
+            #region Using ViewBag
+            //IEnumerable<SelectListItem> list = _db.Villas.ToList().Select(u => new SelectListItem
+            //{
+            //    Text = u.Name,
+            //    Value = u.Id.ToString()
+            //});
+            ////ViewData["VillaList"] = list;//Viewdata is to move data from controller to view
+            //ViewBag.VillaList = list;
+            //return View();
+            #endregion
+
+            #region Using VM
+            VillaNumberVM villaNumberVM = new VillaNumberVM()
             {
-                Text = u.Name,
-                Value = u.Id.ToString()
-            });
-            //ViewData["VillaList"] = list;//Viewdata is to move data from controller to view
-            ViewBag.VillaList = list;
-            return View();
+                VillaList = _db.Villas.ToList().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                })
+            };
+            #endregion
+            return View(villaNumberVM);
 
             //return View("Index");
         }
 
         [HttpPost]
-        public IActionResult Create(VillaNumber obj)
+        public IActionResult Create(VillaNumberVM obj)
+
         {
             //if (obj.Name == obj.Description)
             //{
@@ -56,17 +75,40 @@ namespace WhiteLagoon.Web.Controllers
             //< FrameworkReference Include = "Microsoft.AspNetCore.App" ></ FrameworkReference >
             //</ ItemGroup >
             #endregion
-            
-            if (ModelState.IsValid)
+
+            //if (ModelState.IsValid)
+            //{
+            //    _db.VillaNumbers.Add(obj);
+            //    _db.SaveChanges();
+            //    TempData["success"] = "The villa number has been create successfully.";
+            //    return RedirectToAction("Index", "VillaNumber");
+            //    //return RedirectToAction("Index");
+            //    //return View("Index"); //Error here
+            //}
+
+            bool roomNumberExist = _db.VillaNumbers.Any(u => u.Villa_Number == obj.VillaNumber.Villa_Number);
+            //bool isNumberUnique = _db.VillaNumbers.Where(u => u.Villa_Number == obj.Villa_Number).Count()==0;
+            if (ModelState.IsValid && !roomNumberExist)
             {
-                _db.VillaNumbers.Add(obj);
+                
+                _db.VillaNumbers.Add(obj.VillaNumber);
                 _db.SaveChanges();
                 TempData["success"] = "The villa number has been create successfully.";
                 return RedirectToAction("Index", "VillaNumber");
                 //return RedirectToAction("Index");
                 //return View("Index"); //Error here
             }
-            TempData["error"] = "The villa has number not been create successfully.";
+            else
+            {
+                TempData["error"] = "The villa number has already Exist";
+                obj.VillaList = _db.Villas.ToList().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                });
+            }
+
+            
             return View(obj);
         }
         #endregion
@@ -75,36 +117,48 @@ namespace WhiteLagoon.Web.Controllers
         [HttpGet]
         public IActionResult Update(int? villaNumberId)
         {
-            Villa? obj = _db.Villas.FirstOrDefault(needVilla => needVilla.Id == villaNumberId);
+            #region Using VM
+            VillaNumberVM villaNumberVM = new VillaNumberVM()
+            {
+                VillaList = _db.Villas.ToList().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                }),
+                VillaNumber=_db.VillaNumbers.FirstOrDefault(u=>u.Villa_Number== villaNumberId)
+            };
+            #endregion
+            //Villa? obj = _db.Villas.FirstOrDefault(needVilla => needVilla.Id == villaNumberId);
             //Villa? obj2 = _db.Villas.Find(VillaId);
             //var villarList = _db.Villas.Where(u => u.Price > 50 && u.Occupancy > 4).ToList();
-            if (villaNumberId == null)
+            if (villaNumberVM.VillaNumber == null)
             {
                 //return NotFound();   
                 return RedirectToAction("Error", "Home");
             }
 
-            return View(obj);
+            return View(villaNumberVM);
         }
         [HttpPost]
-        public IActionResult Update(VillaNumber obj)
+        public IActionResult Update(VillaNumberVM obj)
         {
-            //Custome Validation
-            //if (obj.Name == obj.Description)
-            //{
-            //    //ModelState.AddModelError("description", "The description cannot exactly match the Name.");
-            //    ModelState.AddModelError("", "The description cannot exactly match the Name.");
-            //}
+            //bool roomNumberExist = _db.VillaNumbers.Any(u => u.Villa_Number == obj.VillaNumber.Villa_Number);
+            //bool isNumberUnique = _db.VillaNumbers.Where(u => u.Villa_Number == obj.Villa_Number).Count()==0;
             if (ModelState.IsValid)
             {
-                _db.VillaNumbers.Update(obj);
+
+                _db.VillaNumbers.Update(obj.VillaNumber);
                 _db.SaveChanges();
-                TempData["success"] = "The villa number has been updated successfully.";
+                TempData["success"] = "The villa number has been update successfully.";
                 return RedirectToAction("Index", "VillaNumber");
                 //return RedirectToAction("Index");
                 //return View("Index"); //Error here
             }
-            TempData["error"] = "The villa number hasn't been updated successfully.";
+            obj.VillaList = _db.Villas.ToList().Select(u => new SelectListItem
+            {
+                Text = u.Name,
+                Value = u.Id.ToString()
+            });
             return View(obj);
         }
         #endregion
