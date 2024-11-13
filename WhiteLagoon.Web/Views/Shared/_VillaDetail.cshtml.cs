78a07112ef6c4ad7883ef6bdd4bf9288b7befb9e
nguyentuananh921 <nguyentuananh921@gmail.com>
2024-11-13 13:25:29 +0700

    Section 10 - Assignment 4 Solution - Partial View

---
+++ WhiteLagoon.Web/Views/Shared/_VillaDetail.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WhiteLagoon.Web.Views.Shared
+{
+    public class _VillaDetailModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
